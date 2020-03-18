6681f497f3f3ebd45aaff74b43d5bbc428aef913
Abdul Haseeb <abdulhaseeb13mar@gmail.com>
2020-03-19 02:43:20 +0500

    forcefull inital authorization added  and some code organization

--- App.js
+++ App.js
@@ -7,12 +7,11 @@
  */
 import 'react-native-gesture-handler';
 import React, {useState, useEffect} from 'react';
+import {AsyncStorage} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-// import home from './Component/home';
 import Login from './Component/LoginScreen/src/screens/loginScreen';
 import Signup from './Component/SignupScreen/src/screens/signupScreen';
-import {AsyncStorage} from 'react-native';
 import HomeScreen from './HomeNavigations';
 import SplashScreen from './Component/splashScreen';
 
@@ -27,15 +26,15 @@ const App: () => React$Node = () => {
   const fetchUserToken = async () => {
     let userinfo = {};
     await AsyncStorage.multiGet(['name', 'email', 'uid'], (error, stores) => {
-      if (error === null) {
-        stores.map((_, i, store) => {
-          userinfo = {...userinfo, [store[i][0]]: store[i][1]};
-        });
-        setCurrentUser(userinfo);
-        setIsLoading(false);
-      } else {
-        console.log('fetch data erorr:', error);
+      if (error) {
+        console.log(error);
+        return;
       }
+      stores.map((_, i, store) => {
+        userinfo = {...userinfo, [store[i][0]]: store[i][1]};
+      });
+      setCurrentUser(userinfo);
+      setIsLoading(false);
     });
   };
 
